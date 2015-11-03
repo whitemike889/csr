bcd4fc58cf1c9ce7fe5716d9e856d1d500de2e5a
joseph <seidel.jp@gmail.com>
2015-11-03 11:11:34 -0600

    bump

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("MYAPP_DEBUG", False)
 
-ALLOWED_HOSTS = ["pure-temple-6270.herokuapp.com",]
+ALLOWED_HOSTS = [".herokuapp.com",]
 
 
 # Application definition
