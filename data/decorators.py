cae7c153ba829406d9171c4c7724a35a023f3f85
joseph <seidel.jp@gmail.com>
2016-02-05 15:18:52 -0600

    added measures against spamming

--- data/decorators.py
+++ data/decorators.py
@@ -1,5 +1,14 @@
 from .models import Image, Task, WorkTimer, EventLog
 from django.shortcuts import render
+from django.utils import timezone
+import datetime
+
+def check_for_spam(user_id, seconds):
+    worktimer = WorkTimer.objects.filter(user_id=user_id).order_by('-timestamp')[0]
+    if timezone.now() - worktimer.timestamp < datetime.timedelta(0,30) and int(seconds) == int(worktimer.value):
+        return True
+    else:
+        return False
 
 def timeout_logging(view_func):
     def _wrapped_view_func(request, image_id, *args, **kwargs):
@@ -8,7 +17,8 @@ def timeout_logging(view_func):
         if request.method == "POST":
             print request.POST['token']
             print request.POST['seconds']
-            worktimer, created = WorkTimer.objects.get_or_create(user_id=request.user.id, value=request.POST['seconds'], token=request.POST['token'])
+            if not check_for_spam(request.user.id, request.POST['seconds']):
+                worktimer, created = WorkTimer.objects.get_or_create(user_id=request.user.id, value=request.POST['seconds'], token=request.POST['token'])
             task = Task.objects.get(user_id=request.user.id, image_id=image_id)
             eventlog = EventLog(task_id=task.id, name=request.POST['action'])
             eventlog.save()
