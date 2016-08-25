a09fe5ebb463df02f3a1ec76122c811015e97f9a
Joseph <seidel.jp@gmail.com>
2016-08-25 15:50:50 -0500

    added a message box

--- data/models.py
+++ data/models.py
@@ -16,8 +16,8 @@ def get_now():
 class Constants:
     workdates = {
         '0': {
-            'start': datetime.datetime(2016, 7, 11, 0, 01),
-            'end': datetime.datetime(2016, 7, 14, 23, 59),
+            'start': datetime.datetime(2016, 8, 24, 0, 01),
+            'end': datetime.datetime(2016, 8, 27, 23, 59),
             },
         '1': {
             'start': datetime.datetime(2016, 7, 12, 0, 01),
